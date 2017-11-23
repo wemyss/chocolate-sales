3def2d526c75beaa133b1b65e38d1e57e8942cad
Sam Wemyss <samuel.wemyss@gmail.com>
2017-11-23 20:35:49 +1100

    :sparkles: add dynamodb connection to stop sending too many emails. Loop through email list to get around mailgun limits

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+import boto3
 import json
 import os
 import requests
 
-def send_email(amount):
-	API_KEY = os.environ.get('API_KEY')
-	DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
-	MAILING_LIST = os.environ.get('MAILING_LIST')
+SALE_KEY = 'wasOnSale'
+MAX_SALE_PRICE = 4
 
-	if not (API_KEY and DOMAIN_NAME and MAILING_LIST):
+def send_email(amount):
+	API_KEY = os.environ.get('MAILING_LIST')
+	DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
+	EMAIL_LIST = os.environ.get('EMAIL_LIST')
+
+	if not (API_KEY and DOMAIN_NAME and EMAIL_LIST):
 		print("Invalid environment variables")
 		return
 
-	return requests.post(
-		("https://api.mailgun.net/v3/%s/messages" % DOMAIN_NAME),
-		auth = ("api", API_KEY),
-		data = {
-			"from": ("Chocolate sales <mailgun@%s>" % DOMAIN_NAME),
-		  	"to": MAILING_LIST,
-		  	"subject": ("Chocolate Sale | $%s" % amount),
-		  	"text": ("It's on sale at Coles! The price is $%s" % value)
-		}
-	)
+	for email in EMAIL_LIST.split(','):
+		print(email)
+		result = requests.post(
+			("https://api.mailgun.net/v3/%s/messages" % DOMAIN_NAME),
+			auth = ("api", API_KEY),
+			data = {
+				"from": ("Chocolate sales <chocolate-sales@%s>" % DOMAIN_NAME),
+				"to": email,
+				"subject": ("Chocolate Sale | $%s" % amount),
+				"text": ("It's on sale at Coles! The price is $%s" % value)
+			}
+		)
+		print(result.text)
+
 
 def get_chocolate_price():
 	# TODO: Add error handling and notification system for when this URL fails
@@ -31,19 +39,58 @@ def get_chocolate_price():
 	return float(data['catalogEntryView'][0]['p1']['o'])
 
 
+
 ############
 ### MAIN ###
 ############
+db = boto3.resource(
+	'dynamodb',
+	aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
+	aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
+	region_name = 'ap-southeast-2'
+)
+table = db.Table('chocolate-sales')
+
 
 value = get_chocolate_price()
+isOnSale = value < MAX_SALE_PRICE
+wasOnSale = False
 
-if value < 4:
-	print("It's on sale! The price is $%s" % value)
-	# TODO: Only send an email once per sale period
-	print(send_email(value))
+
+# Get previous sale check
+try:
+	response = table.get_item(
+		Key = {
+			'name': SALE_KEY
+		}
+	)
+
+except ClientError as e:
+	print(e.response['Error']['Message'])
+
+else:
+	wasOnSale = response['Item']['value']
+
+
+# If sale just started send an email
+if isOnSale:
+	if wasOnSale:
+		print("It's been on sale for a while now. skip email alert")
+	else:
+		print("It's on sale! The price is $%s" % value)
+		send_email(value)
+
 else:
 	print("No sale :( price is $%s" % value)
 
+
+# Update the db with latest sale check
+table.put_item(
+	Item = {
+		'name': SALE_KEY,
+		'value': isOnSale,
+	}
+)
 
 
 
