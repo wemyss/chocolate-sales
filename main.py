81c3f2e4c2ee3394f02373197569c98239de2a0b
Sam Wemyss <samuel.wemyss@gmail.com>
2018-04-21 23:06:07 +1000

    :tada: v2 sales notifier now more generic ever before

--- main.py
+++ main.py
@@ -2,96 +2,158 @@ import boto3
 import json
 import os
 import requests
-
-SALE_KEY = 'wasOnSale'
-MAX_SALE_PRICE = 4
-
-def send_email(amount):
-	API_KEY = os.environ.get('API_KEY')
-	DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
-	EMAIL_LIST = os.environ.get('EMAIL_LIST')
-
-	if not (API_KEY and DOMAIN_NAME and EMAIL_LIST):
-		print("Invalid environment variables")
-		return
-
-	for email in EMAIL_LIST.split(','):
-		print(email)
-		result = requests.post(
-			("https://api.mailgun.net/v3/%s/messages" % DOMAIN_NAME),
-			auth = ("api", API_KEY),
-			data = {
-				"from": ("Chocolate sales <chocolate-sales@%s>" % DOMAIN_NAME),
-				"to": email,
-				"subject": ("Chocolate Sale | $%s" % amount),
-				"text": ("It's on sale at Coles! The price is $%s" % value)
-			}
-		)
-		print(result.text)
+from decimal import *
 
 
-def get_chocolate_price():
-	# TODO: Add error handling and notification system for when this URL fails
-	r = requests.get('https://shop.coles.com.au/search/resources/store/20601/productview/bySeoUrlKeyword/green-blacks-organic-85%25-dark-chocolate?catalogId=10576')
-
-	data = r.json()
-
-	return float(data['catalogEntryView'][0]['p1']['o'])
-
-
-
-############
-### MAIN ###
-############
+# DYNAMO DB SETUP & HELPERS
+# ===========================================================
 db = boto3.resource(
 	'dynamodb',
 	aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
 	aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
 	region_name = 'ap-southeast-2'
 )
-table = db.Table('chocolate-sales')
+items_table = db.Table('sams-sales')
+users_table = db.Table('sams-sales-users')
 
+def price_is_lower_than_last_time(name, price):
+	'''
+	Return true if current price for the item is less than previous price, else false.
+	'''
+	try:
+		response = items_table.get_item(
+			Key = {
+				'name': name,
+			}
+		)
+	except ClientError as e:
+		print(e.response['Error']['Message'])
+	else:
+		if 'Item' in response:
+			prev_price = response['Item']['price']
+			return price < prev_price
+		else:
+			return False
 
-value = get_chocolate_price()
-isOnSale = value < MAX_SALE_PRICE
-wasOnSale = False
-
-
-# Get previous sale check
-try:
-	response = table.get_item(
-		Key = {
-			'name': SALE_KEY
+def update_price(name, price):
+	'''
+	Update the previous price of the item
+	'''
+	items_table.put_item(
+		Item = {
+			'name': name,
+			'price': Decimal(str(price)),
 		}
 	)
 
-except ClientError as e:
-	print(e.response['Error']['Message'])
-
-else:
-	wasOnSale = response['Item']['value']
+def get_users():
+	return users_table.scan()['Items']
 
 
-# If sale just started send an email
-if isOnSale:
-	if wasOnSale:
-		print("It's been on sale for a while now. skip email alert")
-	else:
-		print("It's on sale! The price is $%s" % value)
-		send_email(value)
+# GATHER ITEM PRICES
+# ===========================================================
+def get_price_coles(url):
+	r = requests.get(url)
+	data = r.json()
+	return float(data['catalogEntryView'][0]['p1']['o'])
 
-else:
-	print("No sale :( price is $%s" % value)
+def get_price_woolworths(url):
+	r = requests.get(url)
+	data = r.json()
+	return float(data['Product']['Price'])
+
+def get_cheaper_vendor(prices):
+	cheaper_vendor = None
+	for vendor, price in prices.items():
+		if cheaper_vendor is None or price < prices[cheaper_vendor]:
+			cheaper_vendor = vendor
+	return cheaper_vendor
+
+def get_items_on_sale(data):
+	sale_items = []
+
+	for item in data['items']:
+		prices = {}
+
+		# handle empty urls for coles or woolies
+		if 'coles_url' in item:
+			prices['coles'] = get_price_coles(data['coles_url_prefix'] + item['coles_url'])
+		if 'woolworths_url' in item:
+			prices['woolworths'] = get_price_woolworths(data['woolworths_url_prefix'] + item['woolworths_url'])
 
 
-# Update the db with latest sale check
-table.put_item(
-	Item = {
-		'name': SALE_KEY,
-		'value': isOnSale,
-	}
-)
+		cheaper_vendor = get_cheaper_vendor(prices)
+		price = prices[cheaper_vendor]
+
+		if price_is_lower_than_last_time(item['name'], price):
+			sale_items.append({ 'name': item['name'], 'price': price, 'vendor': cheaper_vendor })
+
+		update_price(item['name'], price)
+
+	return sale_items
+
+
+# HTML & EMAIL
+# ===========================================================
+with open('email-template.html', 'r') as myfile:
+    _html_email_template = myfile.read().replace('\n', '').replace('\t', '')
+
+def generate_email(sale_items):
+	table_html = ''
+
+	for item in sale_items:
+		table_html += ('<tr><td>' + item['name'] + '</td>'
+			'<td>$' + ('%.2f' % item['price']) + '</td>'
+			'<td>' + item['vendor'].title() + '</td></tr>')
+
+	return _html_email_template.replace('<!--INSERT_HERE-->', table_html, 1)
+
+def send_email(email, body):
+	API_KEY = os.environ.get('API_KEY')
+	DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
+
+	if not (API_KEY and DOMAIN_NAME):
+		print('Invalid environment variables')
+		return
+
+	result = requests.post(
+		('https://api.mailgun.net/v3/%s/messages' % DOMAIN_NAME),
+		auth = ('api', API_KEY),
+		data = {
+			'from': ('Sams Sales <sams-sales@%s>' % DOMAIN_NAME),
+			'to': email,
+			'subject': 'Stuff is on sale',
+			'html': body
+		}
+	)
+	print(result.text)
+
+
+def user_wants_item(user, item):
+	'''
+	Return true if any item_keywords for the user are in the item name.
+	'''
+	return any(keyword in item['name'] for keyword in user['item_keywords'])
+
+def notify_users(sale_items):
+	users = get_users()
+
+	for user in users:
+		user_interested_items = [item for item in sale_items if user_wants_item(user, item)]
+		if len(user_interested_items) > 0:
+			send_email(user['email'], generate_email(user_interested_items))
+			# print(user['email'] + '   ' + generate_email(user_interested_items) + '\n')
 
 
 
+# MAIN
+# ===========================================================
+def main():
+	data = json.load(open('items.json'))
+	sale_items = get_items_on_sale(data)
+	if len(sale_items) > 0:
+		notify_users(sale_items)
 
+
+
+main()
